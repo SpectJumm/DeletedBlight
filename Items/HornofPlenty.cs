2d212279c09ccc7fa927c7bea318b1a422e0d8c5
YT_SpectJumm <141079307+SpectJumm@users.noreply.github.com>
2026-06-20 12:25:21 -0400

    modified:   Items/HornofPlenty.cs

--- Items/HornofPlenty.cs
+++ Items/HornofPlenty.cs
@@ -4,15 +4,14 @@ using Terraria.ModLoader;
 
 namespace DeletedBlight.Items
 {
-    public class HornofPlenty : ModItem
+    public class HornofPlenty : ModItem // Gold named the sprite "Horn of Nasty" lol, the name must not be lost to the sands of time.
     {
         public override void SetDefaults() 
 		{
             // These variables are orange for an AMAZING reason!
-            // No seriously why are they orange?
-            Item.width = 17;
-            Item.height = 17;
-            Item.scale = 2f;
+            // No seriously why are they orange? I have no clue and I don't like it.
+            Item.width = 34; // TODO: Change the size of the sprite to these dimensions.
+            Item.height = 34;
             Item.useStyle = ItemUseStyleID.DrinkLiquid;
             Item.useAnimation = 17;
             Item.useTime = 17;
@@ -22,11 +21,11 @@ namespace DeletedBlight.Items
             Item.consumable = false;
             Item.rare = ModContent.RarityType<Rarities.BlightGreen>();
             Item.value = Item.buyPrice(gold: 25);
-            Item.potion = true; // Counts as a potion for the potion sickness debuff. Why doesn't it work with Quick Heal tho???
+            Item.potion = true; // Counts as a potion for the potion sickness debuff.
 			Item.expert = true; // Only available in Expert mode
 		}
 
-        public override void GetHealLife(Player player, bool quickHeal, ref int healValue)
+        public override void GetHealLife(Player player, bool quickHeal, ref int healValue) // This SHOULD make it work with Quick Heal.
         {
             healValue = 250; // Ensure it heals 250 life regardless of other factors
         }
